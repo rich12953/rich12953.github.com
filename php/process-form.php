3b9770449e2be7d639c95b8956f6463b575d0c9b
Ricardo Rangel <rrangel@bitevolution.net>
2019-01-19 14:12:20 -0600

    correo

---
+++ php/process-form.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rich
+ * Date: 1/19/2019
+ * Time: 1:50 p.m.
+ */
+
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$nombre = $_POST['comment'];
+
+echo $nombre;
+?>
+
---
+++ process-form.php
@@ -0,0 +1,17 @@
+<?php
+
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$comentario = $_POST['comment'];
+
+$subject =  'Nuevo mensaje de Portafolio de ' . $email;
+
+echo $nombre . ' ' . $email . ' ' . $comentario;
+
+if(mail('rich12953@gmail.com', $subject, $comentario)){
+    echo "mensaje enviado";
+} else {
+    echo "No se ha podido enviar el mensaje";
+}
+?>
+
